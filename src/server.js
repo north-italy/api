83b1b13ac1461b80b8c5399ee1b74b4a6513b927
north-italy <simon.mair98@gmail.com>
2025-05-15 00:29:39 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -15,8 +15,8 @@ export default async function handler(req, res) {
          { id: 3, name: 'Charlie Brown', email: 'charlie@example.com' }],
         { status: 200, statusText: "OK" });
     default:
-      return res.json(
+      return res.status(404).json(
         { message: 'Endpoint not found' },
-        { status: 404, statusText: "Not Found" });
+        { statusText: "Not Found" });
   }
 }
