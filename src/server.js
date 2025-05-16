0782588e2e4673ddeebf7f9af5e974fe90faf22d
north-italy <simon.mair98@gmail.com>
2025-05-16 23:57:56 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -34,7 +34,7 @@ export default async function handler(req, res) {
         
         let reqBody = {some: null};
         try {
-          reqBody = await req.json();
+          reqBody = await requestBody.json();
         } catch(err) {
           res.status(400).json({ message: 'Failed to parse body' });
           return; // Wichtig: Beende die Funktion nach dem Senden der Antwort
