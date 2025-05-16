1f03cfb572985fab2da69167585926304213aecb
north-italy <simon.mair98@gmail.com>
2025-05-16 23:30:56 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -24,8 +24,9 @@ export default async function handler(req, res) {
       } else {
         try {
           let message = 'Body is good';
-          let reqBody = await req.json();
-
+          //let reqBody = await req.json();
+          const json = await new Response(req.body).json();
+          
           const requiredParams = new Map([
             ['token', 'string'],
             ['value', 'number']
