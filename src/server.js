238e6c5595aab2c102a7f52e12a97bbf00fabe95
north-italy <simon.mair98@gmail.com>
2025-05-22 01:55:46 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -52,7 +52,7 @@ export default async function handler(req, res) {
       } else {
 try {
 const {token, value} = req.body;
-return res.status(200).json({ message: "All good", value: value }, { statusText: "OK" });
+return res.status(200).json({ message: `All good. Your value is: [${value}]` }, { statusText: "OK" });
 } catch (err) {
 return res.status(404).json({ message: 'Error occurred' });
 }
